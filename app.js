64317647bb947d2c32ae7949ce6a72a213e57938
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-19 05:29:06 +0100

    added new routes, cors dotenv

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
+dotenv.config()
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const wordRouter = require('./routes/word');
 
 const app = express();
+app.use(cors());
+
+
+app.use(cookieParser());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/word', wordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
