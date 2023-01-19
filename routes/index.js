14fc141ba10c541114f4ce27726840d624723fb8
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-19 05:23:09 +0100

    added telegram bot API and connected MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+require('../modules/telegram');
+require('../modules/connectMongo');
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
