9d379ddc3257bf2107726146fe5d4a7c2eef4574
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-19 05:39:11 +0100

    added user schema mongoose

---
+++ models/UserTelegram.js
@@ -0,0 +1,14 @@
+const {Schema,model} = require('mongoose');
+
+const user = new Schema({
+    id : {
+        type:Schema.Types.Number,
+        ref: 'Word'
+    },
+    first_name: Schema.Types.String,
+    last_name: Schema.Types.String,
+    username: Schema.Types.String,
+    password: Schema.Types.String,
+});
+
+module.exports = model('User',user);
