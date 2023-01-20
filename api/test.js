3b3c36f073b928fbb4c17df0eec25c3e6b492092
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-20 05:58:16 +0100

    tested "populate" and added method getAllTestsByUserId

--- api/test.js
+++ api/test.js
@@ -15,6 +15,15 @@ class Test {
         let test = new TestModel({title,image,description,words,numberOfTimes,userId})
         return await test.save();
     }
+
+    static async getAllTestsByUserId(userId){
+        return await TestModel.find({userId});
+    }
+    static async getByIdUser(){
+        return await TestModel.find({userId: 556451309}).populate('words').exec((err,res) =>{
+            console.log(res[0].words)
+        });
+    }
 }
 
 module.exports = Test;
