a2404a230913ecc5e5a101985a735a8d24e3d782
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-20 05:55:44 +0100

    added reference for array of words

--- models/TestInputText.js
+++ models/TestInputText.js
@@ -4,7 +4,12 @@ const textInput = new Schema({
     title: Schema.Types.String,
     image: Schema.Types.String,
     description: Schema.Types.String,
-    words: {},
+    words: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Word'
+        }
+    ],
     numberOfTimes: Schema.Types.Number,
     userId: {
         type: Schema.Types.Number,
