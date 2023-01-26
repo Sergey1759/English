c6d4a40ebac497d8f8b721431337b001eb9f1df2
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-01-26 15:48:03 +0100

    added testing by radio buttons

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const ApiWords = require('../api/word');
 const ApiTest = require('../api/test');
 const ApiResult = require('../api/result');
+const ApiResultRadio = require('../api/result-radio');
 const isAuthenticated = require('./middleware/isAuthenticated');
+
+const getImages = require('../modules/puppeteer/getImagesFromUnsplash');
 
 router.get('/createTest', isAuthenticated,async function(req, res, next) {
     const allWords = await ApiWords.getByUserId(req.user.id);
@@ -21,8 +24,16 @@ router.get('/testingText/:id', isAuthenticated,async function(req, res, next) {
     res.render('test-pages/testing-text', { title: 'Express' , JsonWords,idTest : test._id});
 });
 
+router.get('/testingRadio/:id', isAuthenticated,async function(req, res, next) {
+    const test = await ApiTest.getByIdUserAndTestID(req.user.id, req.params.id);
+    let JsonWords = JSON.stringify(test.words);
+    res.render('test-pages/testing-radio', { title: 'Express' , JsonWords,idTest : test._id});
+});
+
 router.post('/createTest', isAuthenticated, async function(req, res, next) {
-    const {title,imageUrl,description,words} = req.body;
+    const {title,description,words} = req.body;
+    let images = await getImages('exam');
+    const imageUrl = images[Math.floor(Math.random() * ((images.length-1) - 0 + 1) + 0)];
     let test = new ApiTest(title,imageUrl,description,words,req.user.id)
     await test.createTest();
     res.send({ title: 'Express' });
@@ -32,8 +43,12 @@ router.post('/checkTest', isAuthenticated,async function(req, res, next) {
     let test = await ApiTest.CheckResult(req.user.id,id,data);
     let result = new ApiResult('text',id,test,req.user.id);
     const response = await result.create();
-    console.log(response)
     res.send({ id: response._id.toString() });
 });
 
+router.post('/checkTestRadio', isAuthenticated,async function(req, res, next) {
+    let radio =  new ApiResultRadio('radio',req.body.idTest,req.body.questions,req.user.id);
+    let r = await radio.create();
+    res.send({ id : r._id});
+});
 module.exports = router;
