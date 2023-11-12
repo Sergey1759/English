6aee61fb75ee576e76fb8071f6134d8405c378f4
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-11-12 13:07:56 +0100

    added images for story

---
+++ modules/AI/promptForStory.js
@@ -0,0 +1,31 @@
+module.exports = `
+
+write new story and give it in json format like here 
+
+{
+  "chapters": [
+    {
+      "chapter_number": 1,
+      "title": "The Enchanted Forest",
+      "content": "In the heart of the mystical land of Eldoria, where ancient secrets whispered through the leaves of towering trees and magical creatures roamed freely, lived a young adventurer named Aria. Aria, with her emerald-green eyes and auburn hair, possessed an insatiable curiosity that often led her into the unknown.\\n\\nOne day, while exploring the outskirts of her village, Aria stumbled upon a hidden path that seemed to beckon her into the depths of the Enchanted Forest. Intrigued by the faint glow that emanated from within, she hesitated for only a moment before deciding to venture into the mysterious woodland.\\n\\nAs she delved deeper into the forest, the air grew thick with enchantment, and the trees whispered secrets of forgotten realms. Aria felt an otherworldly presence guiding her, and soon, she stumbled upon a clearing bathed in ethereal light. In the center stood an ancient tree, its branches adorned with shimmering crystals that hummed with magic.\\n\\nSuddenly, the ground trembled, and from the roots of the ancient tree emerged a mystical creature - a talking fox named Lumina. Lumina, with her silver fur and wise eyes, revealed that Aria was chosen to embark on a quest to restore balance to Eldoria, which was threatened by an ancient darkness that sought to consume the magic that sustained the land."
+    },
+    {
+      "chapter_number": 2,
+      "title": "The Quest Unfolds",
+      "content": "With Lumina as her guide, Aria accepted the responsibility bestowed upon her. The talking fox explained that the only way to save Eldoria was to retrieve the lost Crystal of Harmony, a powerful artifact hidden in the Forbidden Caverns beyond the Whispering Peaks.\\n\\nTogether, Aria and Lumina journeyed through the Enchanted Forest, facing magical challenges and befriending creatures that pledged their allegiance to their quest. Along the way, Aria discovered her latent magical abilities, which grew stronger with every obstacle they overcame.\\n\\nAs they reached the base of the Whispering Peaks, the duo encountered the Guardian Spirits, ancient beings tasked with protecting the entrance to the Forbidden Caverns. To prove her worth, Aria had to pass a series of trials that tested her courage, wisdom, and purity of heart.\\n\\nSuccessfully passing the trials, Aria and Lumina entered the Forbidden Caverns, a labyrinthine network of tunnels filled with traps and illusions. With Lumina's guidance and Aria's newfound powers, they navigated the treacherous caverns, inching closer to the chamber where the Crystal of Harmony awaited.\\n\\nLittle did they know that the ancient darkness sensed their approach and sent shadowy minions to thwart their progress. As they faced the final challenge before reaching the crystal, Aria and Lumina braced themselves for a confrontation that would determine the fate of Eldoria and the future of magic itself."
+    }
+  ],
+  "image_prompts": [
+    {
+      "chapter_number": 1,
+      "title": "The Enchanted Forest - Image Prompt",
+      "description": "Illustrate the mesmerizing scene as Aria discovers the hidden path leading into the Enchanted Forest. Captivate the viewer with the vibrant colors of the mystical flora and the towering, ancient trees. Showcase Aria's curiosity and trepidation as she takes the first step into the unknown. The air should shimmer with enchantment, and the mystical creatures lurking in the shadows should be hinted at, adding an element of mystery to the image. The focal point should be the moment Aria stumbles upon the clearing with the ancient, crystal-adorned tree, capturing the essence of magic that surrounds her."
+    },
+    {
+      "chapter_number": 2,
+      "title": "The Quest Unfolds - Image Prompt",
+      "description": "Create an image that depicts Aria and Lumina's journey through the Enchanted Forest, emphasizing their camaraderie and the magical challenges they face. Showcase Aria's growth as a magical adventurer, with subtle visual cues highlighting her newfound abilities. As they approach the Whispering Peaks, convey the grandeur and mystical aura of the Guardian Spirits, emphasizing the trials that Aria must overcome. Transition the image into the Forbidden Caverns, capturing the treacherous nature of the tunnels and the imminent confrontation with the shadowy minions. The image should build anticipation for the pivotal moment when Aria and Lumina stand on the brink of discovering the Crystal of Harmony."
+    }
+  ]
+}
+`;
