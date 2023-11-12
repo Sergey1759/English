5b8e2fde63e2cf8adc7667b2100f27aac1695f92
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2023-11-12 13:01:34 +0100

    added getImage

--- public/javascripts/stories.js
+++ public/javascripts/stories.js
@@ -1,34 +1,43 @@
 let createStory = document.querySelector('#create-story');
 let divStory = document.querySelector('.story');
-let checkBoxes = document.querySelectorAll('input[type="checkbox"]');
-
-for (let checkBox of checkBoxes) {
-    if(checkBox.checked) console.log(checkBox)
-}
 
 createStory.addEventListener('click', async ()=>{
-    let arrayWords = [];
-    for (let checkBox of checkBoxes) {
-        if(checkBox.checked) arrayWords.push(checkBox.value)
-    }
-    let stringWords = arrayWords.join(' ');
+    let {story, src}  = await postData('/stories/' , {words: getWordsForStory()});
 
-    let story  = await postData('/stories/' , {words: stringWords});
-    let buf = story.story.split(' ');
-    console.log(buf);
-    buf = buf.map(word => `<span class="addToVocabulary">${word}</span>`);
-    console.log(buf);
+    let htmlFromText = story.split(' ').map(word => `<span class="addToVocabulary">${word}</span>`);
+
     let div = document.createElement('div');
-    div.innerHTML = buf.join(' ');
+    div.innerHTML = htmlFromText.join(' ');
     divStory.appendChild(div);
+
+    let image = document.createElement('img');
+    image.src = src;
+    divStory.appendChild(image);
+
+    addListenerToWords();
+});
+
+function addListenerToWords() {
     let addToVocabulary = document.querySelectorAll('.addToVocabulary');
+
     for (const addToVocabularyElement of addToVocabulary) {
         addToVocabularyElement.addEventListener('click',async (evt)=>{
             evt.target.style.color = 'red';
             await postData('/stories/createWord/', {word: evt.target.innerText})
         });
     }
-});
+}
+
+function getWordsForStory() {
+    const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+
+    let arrayWords = [];
+    for (let checkBox of checkBoxes) {
+        if(checkBox.checked) arrayWords.push(checkBox.value)
+    }
+    return  arrayWords.join(' ');
+}
+
 
 async function postData(url = '', data = {}) {
     const response = await fetch(url, {
